a8c9fea5367aab740bd351f3778b9210e5040de1
Andrei Naumov <naumov.dron@gmail.com>
2020-08-06 00:34:44 +0300

    added block mass

--- yellow_belt/week_1/block_mass/main.cpp
+++ yellow_belt/week_1/block_mass/main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 
 int main() {
-  int n;
-  uint8_t r;
+  int n, r;
   std::cin >> n >> r;
-  uint64_t mass = 0;
+  uint64_t v = 0;
+  int w, h, d;
   for (int i = 0; i < n; ++i) {
-    
+    std::cin >> w >> h >> d;
+      v += static_cast<uint64_t>(w) * static_cast<uint64_t>(h) * d;
   }
+  std::cout << v * r;
   return 0;
 }
